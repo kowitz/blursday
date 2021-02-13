33e05df9940c3f3a45ad2563a5ad369f541c9a25
Braden Kowitz <kowitz@gmail.com>
2021-02-13 08:12:26 -0800

    Added chrome extension

---
+++ blursday-chrome-extension/script.js
@@ -0,0 +1,10 @@
+day = {
+  0: "Sunday",
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thursday",
+  5: "Friday",
+  6: "Saturday",
+}[new Date().getDay()];
+document.getElementById("day").innerHTML = day;
