a6ebe58e7a28bc231cd28dfbaf8872f46cef7f10
Braden Kowitz <kowitz@gmail.com>
2021-02-20 16:19:19 -0800

    Added Chrome Extension with reduced flickering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react-lite";
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, autorun } from "mobx";
 
 import Home from "./components/Home.js";
 import { colorTheme, progressPeriod } from "./settings";
@@ -14,6 +14,14 @@ class AppState {
 }
 
 const appState = new AppState();
+
+// When embeded in a Chrome Extension new tab,
+// send the background color to the parent window
+autorun(() => {
+  const color = appState.settings.colorTheme.current.backgroundColor;
+  console.log("Posting background color to parent");
+  window.parent.postMessage({ backgroundColor: color }, "*");
+});
 
 const App = observer(() => {
   return (
